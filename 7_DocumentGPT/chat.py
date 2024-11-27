a1e8eb705503eca65aaa48e55ff6af99bb235ad3
radder214 <garden940316@gmail.com>
2024-11-27 19:31:47 +0900

    streamlit 연습

--- 7_DocumentGPT/chat.py
+++ 7_DocumentGPT/chat.py
@@ -1,9 +1,14 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
 
-st.title("Hello world!")
-st.subheader("Welcome Streamlit!")
-st.markdown(
-    """
-    # I love it!
-    """
+# st.write --> 넘겨준 것이 무엇이든 화면에 나타낸다.
+st.write("hello")
+st.write([1, 2, 3, 4])
+st.write({"x" : 1, "y" : 2})
+st.write(PromptTemplate)
+st.write(PromptTemplate.from_template("xxxx"))
+
+st.selectbox(
+    "Choose your model",
+    ("GPT-3", "GPT-4"),
 )
