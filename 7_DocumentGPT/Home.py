9c4429ef7a1406d4e9a689a78779da9ad085b9b1
radder214 <garden940316@gmail.com>
2024-11-29 20:17:42 +0900

    session_state 를 이용해 대화 기록 화면에 렌더링

---
+++ 7_DocumentGPT/Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_icon="🏆",
+    page_title="LLM"
+)
+
+st.markdown("""
+    # Hello!
+            
+    Welcome to my FullstackGPT Portfolio!
+
+    Here are the apps I made:
+
+    - [ ] [DocumentGPT](/DocumentGPT)
+    - [ ] [PrivateGPT](/PrivateGPT)
+    - [ ] [QuizGPT](/QuizGPT)
+    - [ ] [SiteGPT](/SiteGPT)
+    - [ ] [MeetingGPT](/MeetingGPT)
+    - [ ] [InvestorGPT](/InvestorGPT)
+""")
--- 7_DocumentGPT/pages/01_DocumentGPT.py
+++ 7_DocumentGPT/pages/01_DocumentGPT.py
@@ -1,3 +1,47 @@
 import streamlit as st
+import time
 
+st.set_page_config(page_title="Document GPT", page_icon="📖")
 st.title("Document GPT")
+
+# ========== streamlit의 Chat Element에 대해 알아보자 ==========
+# with st.chat_message("human"):
+#     # 해당 block 안에 들어가는 모든 것은 사람이 작성한 것이 된다.
+#     st.write("Hello")
+
+# with st.chat_message("ai"):
+#     # 해당 block 안에 들어가는 모든 것은 ai가 작성한 것이 된다.
+#     st.write("how are you")
+
+# with st.status("Embedding file...", expanded=True) as status:
+#     time.sleep(2)
+#     st.write("Getting the file")
+#     time.sleep(2)
+#     st.write("Embedding the file")
+#     time.sleep(2)
+#     st.write("Caching the file")
+#     status.update(label="Error", state="error")
+
+# ========== AI와 사람 간의 가상 대화를 만들어보자 to 메시지 저장 로직 이해 With session_state ==========
+# st.session_state.messages 생성
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+def send_message(message, role, save=True): # save=True --> default parameter
+    with st.chat_message(role):
+        st.write(message)
+    if save:
+        st.session_state.messages.append({
+            "message" : message,
+            "role"    : role
+        })
+
+# session_state를 이용해 대화 기록 화면에 뿌리기
+for message in st.session_state.messages:
+    send_message(message["message"], message["role"], False)
+
+message = st.chat_input("Send a message to the AI")
+if message:
+    send_message(message, "human")
+    time.sleep(2)
+    send_message(f"You said: {message}", "ai")
